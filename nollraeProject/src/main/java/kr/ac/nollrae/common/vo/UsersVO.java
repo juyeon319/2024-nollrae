dc4f29944ac59a71a20a3d0bc6dcb7f7d7b9ce3a
BOOK-B31RUEMMBD\USER <k.do.dodong@gmail.com>
2024-08-20 19:47:54 +0900

    UsersVO, GuestVO

--- nollraeProject/src/main/java/kr/ac/nollrae/common/vo/UsersVO.java
+++ nollraeProject/src/main/java/kr/ac/nollrae/common/vo/UsersVO.java
@@ -12,16 +12,8 @@ public class UsersVO implements Serializable{
 	private String usersId;
 	//users(게스트+호스트) 비밀번호
 	private String usersPass;
-	//users(게스트+호스트) 이메일
-	private String usersMail;
 	//users(게스트+호스트) 전화번호
 	private String usersTel;
-	//users(게스트+호스트) 우편번호
-	private int usersZip;
-	//users(게스트+호스트) 주소
-	private String usersAddr1;
-	//users(게스트+호스트) 상세주소
-	private String usersAddr2;
 	//users(게스트+호스트) 회원구분
 	private String usersDiv;
 	//users(게스트+호스트) 탈퇴여부
@@ -29,78 +21,45 @@ public class UsersVO implements Serializable{
 
 	private GuestVO guest;
 	private HostVO host;
-	
+
 	public String getUsersId() {
 		return usersId;
 	}
-	
+
 	public void setUsersId(String usersId) {
 		this.usersId = usersId;
 	}
-	
+
 	public String getUsersPass() {
 		return usersPass;
 	}
-	
+
 	public void setUsersPass(String usersPass) {
 		this.usersPass = usersPass;
 	}
-	
-	public String getUsersMail() {
-		return usersMail;
-	}
-	
-	public void setUsersMail(String usersMail) {
-		this.usersMail = usersMail;
-	}
-	
+
 	public String getUsersTel() {
 		return usersTel;
 	}
-	
+
 	public void setUsersTel(String usersTel) {
 		this.usersTel = usersTel;
 	}
-	
-	public int getUsersZip() {
-		return usersZip;
-	}
-	
-	public void setUsersZip(int usersZip) {
-		this.usersZip = usersZip;
-	}
-	
-	public String getUsersAddr1() {
-		return usersAddr1;
-	}
-	
-	public void setUsersAddr1(String usersAddr1) {
-		this.usersAddr1 = usersAddr1;
-	}
-	
-	public String getUsersAddr2() {
-		return usersAddr2;
-	}
-	
-	public void setUsersAddr2(String usersAddr2) {
-		this.usersAddr2 = usersAddr2;
-	}
-	
+
 	public String getUsersDiv() {
 		return usersDiv;
 	}
-	
+
 	public void setUsersDiv(String usersDiv) {
 		this.usersDiv = usersDiv;
 	}
-	
+
 	public String getUsersDel() {
 		return usersDel;
 	}
-	
+
 	public void setUsersDel(String usersDel) {
 		this.usersDel = usersDel;
 	}
-	
-	
+
 }
--- nollraeProject/src/main/java/kr/ac/nollrae/guest/vo/GuestVO.java
+++ nollraeProject/src/main/java/kr/ac/nollrae/guest/vo/GuestVO.java
@@ -15,45 +15,75 @@ public class GuestVO implements Serializable{
 	private String gstGen;
 	//게스트 프로필사진
 	private String gstImg;
-	
+	//게스트 플랫폼
+	private String gstPlatformTp;
+	//게스트 토큰
+	private String gstAccessToken;
+	//게스트 닉네임
+	private String gstNickname;
+
 	public String getGstId() {
 		return gstId;
 	}
-	
+
 	public void setGstId(String gstId) {
 		this.gstId = gstId;
 	}
-	
+
 	public String getGstNm() {
 		return gstNm;
 	}
-	
+
 	public void setGstNm(String gstNm) {
 		this.gstNm = gstNm;
 	}
-	
+
 	public String getGstBir() {
 		return gstBir;
 	}
-	
+
 	public void setGstBir(String gstBir) {
 		this.gstBir = gstBir;
 	}
-	
+
 	public String getGstGen() {
 		return gstGen;
 	}
-	
+
 	public void setGstGen(String gstGen) {
 		this.gstGen = gstGen;
 	}
-	
+
 	public String getGstImg() {
 		return gstImg;
 	}
-	
+
 	public void setGstImg(String gstImg) {
 		this.gstImg = gstImg;
 	}
-	
+
+	public String getGstPlatformTp() {
+		return gstPlatformTp;
+	}
+
+	public void setGstPlatformTp(String gstPlatformTp) {
+		this.gstPlatformTp = gstPlatformTp;
+	}
+
+	public String getGstAccessToken() {
+		return gstAccessToken;
+	}
+
+	public void setGstAccessToken(String gstAccessToken) {
+		this.gstAccessToken = gstAccessToken;
+	}
+
+	public String getGstNickname() {
+		return gstNickname;
+	}
+
+	public void setGstNickname(String gstNickname) {
+		this.gstNickname = gstNickname;
+	}
+
 }
